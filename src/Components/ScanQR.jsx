69bdd9f503a860e018e23f30d8d81d498b2a345e
Anurag Nilawar <anuragnilawar200224@example.com>
2026-03-06 00:24:04 +0530

    chnages camera settings in scan.jsx

--- src/Components/ScanQR.jsx
+++ src/Components/ScanQR.jsx
@@ -13,7 +13,9 @@ export default function ScanQR({ onDetected }) {
             if (result?.[0]?.rawValue) onDetected(result[0].rawValue)
           }}
           onError={(err) => setError(err?.message || 'Camera error')}
-            constraints={{ video : true }}//{ ideal: 'environment' } }}          
+
+          constraints={{ facingMode: { ideal: "environment" } }}
+            // constraints={{ video : true }}//{ ideal: 'environment' } }}          
           styles={{ container: { width: '100%' }, video: { width: '100%' } }}
         />
       </div>
